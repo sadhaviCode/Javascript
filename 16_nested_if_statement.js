b7ad664854bb42f4123ae1faea957f63cf83cf00
sadhaviCode <sadhavi9922@gmail.com>
2026-07-26 20:20:45 +0530

    Add examples of nested if statements, switch statements, for loops, and even number loops

---
+++ 16_nested_if_statement.js
@@ -0,0 +1,12 @@
+let age =20;
+let hasLicense = true;
+
+if (age >= 18) {
+    if (hasLicense) {
+        console.log("You can drive.");
+    } else {
+        console.log("You need a license to drive.");
+    }
+} else {
+    console.log("You are not old enough to drive.");
+}
